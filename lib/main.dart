294ae93d79b1cf19f8f4d43d8ce1cd71fbf2b2f2
문다현 <ansekgus17@naver.com>
2024-04-27 11:26:49 +0900

    e

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'home.dart';
 
 void main() {
   runApp(MyApp());
@@ -8,10 +9,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Navigation Example',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
+      title: 'Flutter Demo',
       home: MainPage(),
     );
   }
@@ -22,93 +20,17 @@ class MainPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Main Page'),
+        title: Text('메인 페이지'),
       ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => SubPage(title: 'Page 1')),
-                );
-              },
-              child: Text('Go to SubPage 1'),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => SubPage(title: 'Page 2')),
-                );
-              },
-              child: Text('Go to SubPage 2'),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => SubPage(title: 'Page 3')),
-                );
-              },
-              child: Text('Go to SubPage 3'),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => SubPage(title: 'Page 4')),
-                );
-              },
-              child: Text('Go to SubPage 4'),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-class SubPage extends StatefulWidget {
-  final String title;
-
-  SubPage({required this.title});
-
-  @override
-  _SubPageState createState() => _SubPageState();
-}
-
-class _SubPageState extends State<SubPage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              '$_counter',
-              style: TextStyle(fontSize: 32.0),
-            ),
-            SizedBox(height: 20.0),
-            ElevatedButton(
-              onPressed: _incrementCounter,
-              child: Text('홈플러스'),
-            ),
-          ],
+        child: ElevatedButton(
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => HomePage()),
+            );
+          },
+          child: Text('홈 페이지로 이동'),
         ),
       ),
     );
